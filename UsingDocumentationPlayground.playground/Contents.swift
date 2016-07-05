6824d644cd3b889ee9a25f69857ba5cd73b1bf0f
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-07-05 16:22:58 -0400

    Crazy how familiar it all feels

---
+++ UsingDocumentationPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+let name = "Audrey"
+name.uppercaseString
+print(name.uppercaseString)
+
+let uppercaseName = name.uppercaseString
+print("\(name)...\(name)...\(uppercaseName)! WAKE UP!")
+
+
+
